a7705bb5073428582ed485caec8fbfc02a46d941
Shrikant Surwase <shrikantsurwase24@gmail.com>
2024-01-22 11:25:16 +0530

    Shellscript

---
+++ Shellscript_Int_02.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#This is creation and calling of shell variable --- defining variables
+
+clss=Young-minds
+Batch=13
+Profession=AWS/DevOps
+echo "Class name is $Class, Batch number $batch, We are learning $Prefession"
+
+apt update -y  # It will update repo 
+apt install openjdk-17-jre -y # It will install Openjdk
